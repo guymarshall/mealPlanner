2d3cc4c7d47bc37ee13d21bfbcde1caba7ffc2f1
Guy Marshall <guymarshall9@gmail.com>
2025-12-23 21:50:38 +0000

    WIP: Initial commit

---
+++ main.c
@@ -0,0 +1,14 @@
+// TODO: Send meal plan e.g. Monday breakfast this, Monday lunch this, list of breakfasts and list of lunches/teas
+// TODO: Some meals are split between days e.g. bolognese on Wednesday tea and Friday tea
+// TODO: Email list every Friday evening at 16:00
+// TODO: EXTENSION: Some meals need more weight so they come up more regularly - some 4x more regular, some 2x more regular, some 1x more regular
+// TODO: EXTENSION: each meal has ingredient list, so list of ingredients needed in email
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello, World!\n");
+
+    return 0;
+}
